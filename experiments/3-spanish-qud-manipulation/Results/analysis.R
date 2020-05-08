bdab8c9b8a09e2370ce0676cb22b197233ca160b
Abimael Hernandez Jimenez <abimaelh@uci.edu>
2020-05-08 11:55:12 -0700

    Experiment 2 and 3 update
    2-added 1/2 images, I still need to change them and add color.
    
    3-added "No" in front of the "none" QUD sentences.

---
+++ experiments/3-spanish-qud-manipulation/Results/analysis.R
@@ -0,0 +1,82 @@
+library(ggplot2)
+library(reshape2)
+library(lme4)
+library(dplyr)
+
+setwd("/Users/Abimael/documents/GitHub/spanish_scope/experiments/1-spanish-goal-manipulation/results/")
+
+source("helpers.r")
+
+d = read.csv("results.csv",header=T)
+
+unique(d$language)
+
+d = d[d$lived=="both8"&d$years=="mostlive",]
+
+# only Spanish as native language
+d = d[d$language!=""&
+        d$language!="Chile"&
+        d$language!="México"&
+        d$language!="Castellano y asturiano",]
+unique(d$language)
+
+length(unique(d$participant_id)) # n=586 (600)
+
+################################
+
+t <- d
+
+e_numeral_s = bootsSummary(data=t[t$quantifier=="numeral",], measurevar="response", groupvars=c("context","number","QUD"))
+
+e_numeral_plot = ggplot(data=e_numeral_s,aes(x=number,y=response,fill=context))+
+  geom_bar(stat="identity",color="black",position=position_dodge())+
+  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=number, width=0.1),position=position_dodge(0.9))+
+  ylim(0,1)+
+  ylab("endorsement rate") +
+  labs(fill="early-success")+
+  facet_wrap(~QUD)+
+  theme_bw()#+
+e_numeral_plot
+#ggsave("spanish-numeral.png")
+
+
+e_quantifier_s = bootsSummary(data=t[t$quantifier=="every",], measurevar="response", groupvars=c("context","QUD"))
+
+e_quantifier_plot = ggplot(data=e_quantifier_s,aes(x=QUD,y=response,fill=context))+
+  geom_bar(stat="identity",color="black",position=position_dodge())+
+  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=QUD, width=0.1),position=position_dodge(0.9))+
+  ylim(0,1)+
+  ylab("endorsement rate") +
+  labs(fill="early-success")+
+  #facet_wrap(~QUD)+
+  theme_bw()#+
+e_quantifier_plot
+#ggsave("spanish-quantifier.png")
+
+
+context_s = bootsSummary(data=t[t$quantifier=="numeral",], measurevar="response", groupvars=c("context","number"))
+
+context_plot = ggplot(data=context_s,aes(x=number,y=response,fill=context))+
+  geom_bar(stat="identity",color="black",position=position_dodge())+
+  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=number, width=0.1),position=position_dodge(0.9))+
+  ylim(0,1)+
+  #labs("order\npreference")+
+  #facet_wrap(~QUD)+
+  theme_bw()#+
+#ggsave("../results/english-numeral-context.png")
+context_plot
+
+
+e_quantifier_no_context_s = bootsSummary(data=d[d$quantifier=="every",], measurevar="response", groupvars=c("QUD"))
+
+e_quantifier_no_context_plot = ggplot(data=e_quantifier_no_context_s,aes(x=QUD,y=response))+
+  geom_bar(stat="identity",color="black",position=position_dodge())+
+  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=QUD, width=0.1),position=position_dodge(0.9))+
+  ylim(0,1)+
+  ylab("endorsement rate") +
+  #labs(fill="early-success")+
+  #facet_wrap(~QUD)+
+  theme_bw()#
+e_quantifier_no_context_plot + theme(text = element_text(size = 35))   
+ggsave("spanish-quantifier-no-context.png")
+
