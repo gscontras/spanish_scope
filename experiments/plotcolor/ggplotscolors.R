bb2f3c2935b59504d5ec57102bb2ff019824d07b
Abimael Hernandez Jimenez <abimaelh@uci.edu>
2021-02-26 11:08:37 -0500

    updated plots
    savinelli predictions for 2 horses,
    
    spanish predictions for 2 horses following savinelli parameters, except for qud priors

--- experiments/plotcolor/ggplotscolors.R
+++ experiments/plotcolor/ggplotscolors.R
@@ -2,12 +2,12 @@
 library(ggplot2)
 
 setwd("~/git/spanish_scope/experiments/plotcolor/")
+setwd("~/GitHub/spanish_scope/experiments/plotcolor/")
 
-
-# Create data
+# Create data for savinelli predictions - 2 items
 data <- data.frame(
   QUD=c("none","many", "all") ,  
-  response=c(0.2811466072455673,0.36634588972698934,0.6349921886602219)
+  response=c(0.21510005748200386,0.3475828246135107,0.6965678833914104)
 )
 #model prediction
 # Barplot
@@ -18,7 +18,7 @@ high_many_qud = ggplot(data, aes(x=reorder(QUD, response),y=response)) +
   xlab("\nQUD")+
   theme_bw()
 high_many_qud #+ theme(text = element_text(size = 25))
-#ggsave("model_pre.png",height=2,width=2.5)
+ggsave("model_pre_2_items.png",height=2,width=2.5)
 ########
 
 # Create data for Spanish
@@ -39,5 +39,27 @@ high_many_qud = ggplot(data, aes(x=reorder(QUD, response2),y=response2)) +
 high_many_qud #+ theme(text = element_text(size = 25))
 #ggsave("spanish_pre.png",height=2,width=2.5)
   
+#colors
+fill = c("#d87609","#b58a08", "#879b04")
+
+##new Spanish 2 items, same QUD priors*******************************************
+# Create data for Spanish
+data <- data.frame(
+  QUD=c("none","many", "all") ,  
+  response2=c(0.3435558276109251
+              ,0.375133174207485
+              ,0.6355697046731988)
+)
+#model prediction
+# Barplot
+high_many_qud = ggplot(data, aes(x=reorder(QUD, response2),y=response2)) + 
+  geom_bar(stat = "identity",fill = c("#d87609","#b58a08", "#879b04"),color ="black", position=position_dodge())+ 
+  ylim(0,1)+
+  ylab("model endorsement\n") +
+  xlab("\nQUD")+
+  theme_bw()
+high_many_qud #+ theme(text = element_text(size = 25))
+#ggsave("spanish_pre_2.png",height=2,width=2.5)
+
 #colors
 fill = c("#d87609","#b58a08", "#879b04")
